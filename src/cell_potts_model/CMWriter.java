4e778815cee9f2642ff7df7584cc6a9c07c3e360
mchchiang <michael_chiang_ch@hotmail.com>
2016-02-20 11:16:48 +0000

    fixed CM writer bug

--- src/cell_potts_model/CMWriter.java
+++ src/cell_potts_model/CMWriter.java
@@ -7,7 +7,7 @@ public class CMWriter extends DataWriter {
 		writer.println();
 		int q = model.getTypesOfSpin();
 		writer.printf("%.8f ", (double) time);
-		for (int i = 1; i <= q; i++){
+		for (int i = 0; i < q; i++){
 			writer.printf("%.8f %.8f ", model.getXCM(i), model.getYCM(i));
 		}		
 	}
