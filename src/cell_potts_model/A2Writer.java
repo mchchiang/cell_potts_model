4c1fc7e1e5183306ad35e3b5c230f82dafb4bbbc
mchchiang <michael_chiang_ch@hotmail.com>
2016-03-03 01:47:17 +0000

    minor update

--- src/cell_potts_model/A2Writer.java
+++ src/cell_potts_model/A2Writer.java
@@ -5,13 +5,13 @@ public class A2Writer extends DataWriter {
 	@Override
 	public void openWriter(String filename){
 		super.openWriter(filename);
-		writer.println("# t alpha2 r4 r4_error");
+		writer.println("# t alpha2 r4");
 	}
 
 	@Override
 	public void writeData(CellPottsModel model, int time) {
 		double [] a2 = model.alpha2();
-		writer.printf("%d %.8f %.8f %.8f\n", time, a2[0], a2[1]);
+		writer.printf("%d %.8f %.8f\n", time, a2[0], a2[1]);
 	}
 
 }
